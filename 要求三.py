4c3e188d54a1d1f7587dbe1b8aef784f05bf3438
Jiann-Ma <72090089+Jiann-Ma@users.noreply.github.com>
2021-03-03 15:52:21 +0800

    Update 要求三.py

--- 要求三.py
+++ 要求三.py
@@ -2,13 +2,11 @@
 #Reference:https://www.runoob.com/python/att-list-max.html、https://newaurora.pixnet.net/blog/post/228238004-python-list%E8%B3%87%E6%96%99%E8%AE%80%E5%8F%96%E3%80%81%E5%A2%9E%E5%8A%A0%E3%80%81%E5%88%AA%E9%99%A4%E3%80%81%E4%BF%AE%E6%94%B9%E6%96%B9%E6%B3%95
 def maxProduct(nums):            #nums是個list
 # 請用你的程式補完這個函式的區塊
-    count = 0
     eachSet = 0
     sumSet = []
     #所有不自我重複的組合
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
-            count += 1
             eachSet = nums[i]*nums[j]
             sumSet.append(eachSet)
             #print(eachSet)               #這時候得出所有六種組合的值
