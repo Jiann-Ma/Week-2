f5dd1ae04f91ea5057a99d8c9216e7b6b4793a65
Jiann-Ma <72090089+Jiann-Ma@users.noreply.github.com>
2021-03-04 16:24:27 +0800

    Add files via upload

---
+++ 要求四.js
@@ -0,0 +1,30 @@
+function twoSum(nums, target){
+// your code here
+    for(i=0; i<nums.length; i++){
+        for(j=i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                console.log('['+i+', '+j+']')
+            }
+        }
+    }
+}
+
+result=twoSum([2, 11, 7, 15], 9)
+console.log(result) // show [0, 2] because nums[0]+nums[2] is 9
+
+//寫法二
+function twoSum(nums, target){
+    // your code here
+        for(i=0; i<nums.length; i++){
+            for(j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    if(i != j){
+                        console.log('['+i+', '+j+']')
+                    }
+                }
+            }
+        }
+    }
+
+result=twoSum([2, 11, 7, 15], 9)
+console.log(result) // show [0, 2] because nums[0]+nums[2] is 9
