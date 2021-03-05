9c63d168576e3bc9c785c245824975653841e023
Jiann-Ma <72090089+Jiann-Ma@users.noreply.github.com>
2021-03-06 01:17:20 +0800

    Update 要求四.py

--- 要求四.py
+++ 要求四.py
@@ -13,7 +13,7 @@ def twoSum(nums, target):
    #for i in range(len(nums)-1)  這也可以，因為j永遠都出現在i後面，所以i部可能是最後一個。     
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
-                print([i, j])
+                return([i, j])
 
 #寫法二
 
@@ -21,7 +21,7 @@ def twoSum(nums, target):
     for i in range(len(nums)):
         for j in range(len(nums)):
             if nums[i] + nums[j] == target and i != j:
-                print([i, j])
+                return([i, j])
 '''
 #寫法三
 def twoSum(nums, target):
@@ -29,8 +29,9 @@ def twoSum(nums, target):
         for i in range(len(nums)):
             x = nums[i]
             if target - x in dict:
-                print([dict[target-x], i])
+                return([dict[target-x], i])
             dict[x] = i
 
 result=twoSum([2, 11, 7, 15], 9)
 print(result) # show [0, 2] because nums[0]+nums[2] is 9
+
