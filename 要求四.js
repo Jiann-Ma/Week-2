af4bd11a9a5eb7647d9e51aa77b78c3425244517
Jiann-Ma <72090089+Jiann-Ma@users.noreply.github.com>
2021-03-06 01:20:55 +0800

    Update 要求四.js

--- 要求四.js
+++ 要求四.js
@@ -3,7 +3,7 @@ function twoSum(nums, target){
     for(i=0; i<nums.length; i++){
         for(j=i+1; j<nums.length; j++){
             if(nums[i] + nums[j] == target){
-                console.log('['+i+', '+j+']')
+                return('['+i+', '+j+']')
             }
         }
     }
@@ -19,7 +19,7 @@ function twoSum(nums, target){
             for(j=i+1; j<nums.length; j++){
                 if(nums[i] + nums[j] == target){
                     if(i != j){
-                        console.log('['+i+', '+j+']')
+                        return('['+i+', '+j+']')
                     }
                 }
             }
